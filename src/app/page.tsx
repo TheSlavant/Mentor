46990ac51da2156b6d6a7a40f20a5875a1829c8d
Yaroslav <shipilov@umich.edu>
2023-09-06 09:45:00 -0700

    Spice it up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,34 +67,53 @@ export default function Home() {
 };
 
   return (
-    <div className="font-sans bg-white p-8 h-screen flex flex-col justify-between">
+    <div style={{ fontFamily: 'Menlo, monospace' }} className="bg-pink-500 p-8 h-screen flex flex-col justify-between text-sm text-justify">
       <div className="mb-6">
-        <header className="text-center text-3xl mb-2">Personality: Paul</header>
-        <p className="text-center text-gray-600">
-          This model is being fine-tuned to be thoughtful, concise, and kind. 
-          It (sometimes) offers good insights and selectively uses sense of humor.
+        <header className="text-center text-white text-3xl mb-2">SoulweightGPT</header>
+        <div className="mx-auto w-3/5 my-8">
+            <p className="text-right text-white mb-1">
+                “To be honest, we didn't know what it meant for a computer to be 'friendly' until Steve told us."
+            </p>
+            <p className="text-right text-white mt-1">
+                — Terry Oyama on building Macintosh, 1984
+            </p>
+        </div>
+        <div className="flex items-center mt-4 mx-auto w-3/5">
+          <p className="text-white mr-4">
+            Help us turn LLMs from THIS
+          </p>
+          <img src="/impesonal_computer.jpg" alt="Impersonal Computer" width={180} height={180}/>
+          <p className="text-white ml-4 mr-4">
+            into THIS
+          </p>
+          <img src="/friendly_macintosh.jpg" alt="Friendly Macintosh" width={180} height={180}/>
+        </div>
+        <p className="text-white mt-4 mx-auto w-3/5">
+          Let's fine-tune this GPT to be thoughtful, curious, kind, and direct like a human. <br />
+          Friendly Macintosh, not Impersonal PC. Friend, not friendly stranger.
         </p>
-        <p className="text-center text-gray-600">
-          DM your chat to @<a href="https://twitter.com/TheSlavant" className="font-semibold text-black underline">TheSlavant</a> so we can keep training.
+        <p className="text-white mt-4 mx-auto w-3/5">
+          This v0.1 is trained a tiny handmade dataset. Let's see just how good we can make it. <br />
+          We're building a better way to eval. For now, just DM your chat @<a href="https://twitter.com/TheSlavant" className="font-semibold underline">TheSlavant</a> and we'll keep fine-tuning.
         </p>
       </div>
-      
-      <div className="shadow-lg bg-gray-100 rounded-lg p-4 flex-grow overflow-y-scroll mb-6 mx-auto w-2/3">
+            
+      <div className="shadow-lg bg-gray-100 rounded-sm p-4 flex-grow overflow-y-scroll mb-6 mx-auto w-3/5">
         {messages.map((message, index) => (
-          <div key={index} className={`p-3 mb-3 ${message.sender === 'user' ? 'bg-white rounded-tl-lg rounded-br-lg' : 'bg-gray-300 rounded-tr-lg rounded-bl-lg'}`}>
+          <div key={index} className={`p-3 mb-3 ${message.sender === 'user' ? 'bg-white rounded-tl-sm rounded-br-sm' : 'bg-gray-300 rounded-tr-sm rounded-bl-sm'}`}>
             {message.text}
           </div>
         ))}
       </div>
       
-      <div className="flex mx-auto w-2/3 mb-4">
+      <div className="flex mx-auto w-3/5 mb-4">
         <input
           value={inputValue}
           onChange={e => setInputValue(e.target.value)}
           placeholder="Type a message..."
-          className="flex-1 p-4 border rounded-l-lg mr-1"
+          className="flex-1 p-4 border rounded-l-sm mr-1"
         />
-        <button onClick={handleSendMessage} className="bg-gray-950 text-white p-4 rounded-r-lg hover:bg-gray-950">
+        <button onClick={handleSendMessage} className="bg-gray-950 text-white p-4 rounded-r-sm hover:bg-gray-950">
           Send
         </button>
       </div>
