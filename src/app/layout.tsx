3aa898fbb6d9f750211ef444642ef7e05323d06c
Yaroslav <shipilov@umich.edu>
2023-09-08 12:24:28 -0700

    Fix open-graph and twitter images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import GoogleAnalytics from "./GoogleAnalytics";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,14 +10,14 @@ export const metadata: Metadata = {
   description: 'SOTA LLMs, but better.',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      {process.env.GA_TRACKING_ID && (
+        <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID} />
+      )}
       <body className={inter.className}>{children}</body>
     </html>
   )
 }
+
